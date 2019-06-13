38fd5d10f4cc3e23c608c81b13c4a61f17178b7f
Jason <jason0liyao@vip.qq.com>
2019-06-13 20:29:29 +0800

    lab3

--- lab3/kern/pmap.c
+++ lab3/kern/pmap.c
@@ -562,8 +562,19 @@ int
 user_mem_check(struct Env *env, const void *va, size_t len, int perm)
 {
 	// LAB 3: Your code here.
-
-	return 0;
+    cprintf("user_mem_check va: %x, len: %x\n", va, len);
+    uint32_t begin = (uint32_t) ROUNDDOWN(va, PGSIZE); 
+    uint32_t end = (uint32_t) ROUNDUP(va+len, PGSIZE);
+    uint32_t i;
+    for (i = (uint32_t)begin; i < end; i += PGSIZE) {
+        pte_t *pte = pgdir_walk(env->env_pgdir, (void*)i, 0);
+        if ((i >= ULIM) || !pte || !(*pte & PTE_P) || ((*pte & perm) != perm)) {        //具体检测规则
+            user_mem_check_addr = (i < (uint32_t)va ? (uint32_t)va : i);                //记录无效的那个线性地址
+            return -E_FAULT;
+        }
+    }
+    cprintf("user_mem_check success va: %x, len: %x\n", va, len);
+    return 0;
 }
 
 //
